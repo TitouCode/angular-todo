d58021acf97489fa496d0bc22e1543d02e71d0c8
titouCode <titouanpierre@orange.fr>
2018-01-26 12:32:22 +0100

    App-front: todo template

---
+++ angular-todo/src/app/todo/todo.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent implements OnInit {
+  todos: any[] = [];
+  pageTitle: string = 'My Todos';
+  constructor() { }
+
+  ngOnInit() {
+    this.todos = [
+      { name: 'buy bread' },
+      { name: 'buy milk'},
+      { name: 'buy eggs'},
+      { name: 'buy corn'},
+      { name: 'buy chips'}
+    ];
+  }
+
+  addElement() {
+    const lastElement = this.todos.length - 1;
+    if (this.todos && !this.todos[lastElement].name) return false;
+    this.todos.push({ name: '' });
+  }
+
+
+  deleteElement(index) {
+    this.todos.splice(index, 1);
+  }
+
+  deleteMulti() {
+    for (let t = this.todos.length -1; t >= 0; t--) {
+      const todo = this.todos[t];
+      if (todo.selected) this.todos.splice(t, 1);
+    }
+  }
+
+}
